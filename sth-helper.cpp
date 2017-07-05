9dd6cd85500995accc472bf2b8faaca7dd7c1d6b
Sun Arthur Ojeda <saojeda@up.edu.ph>
2017-07-06 00:48:17 +0800

    Update sth-helper.cpp
    -Rewritten code to address the issue where sub-directories are not explored
    -Added signal handler to handle cancel event

--- sth-helper.cpp
+++ sth-helper.cpp
@@ -1,58 +1,75 @@
 #include <iostream>
-#include "boost/filesystem.hpp"
+#include <csignal>
+#include <boost/filesystem.hpp>
 
 /*
-A simple CLI utility for GNU/Linux systems* that automates subberthehut's
-search & download process by running it recursively inside a directory 
-containing multiple video files
+--------------------------------
+subberthehut helper (sth-helper)
+--------------------------------
+A simple CLI utility for GNU/Linux systems* that automates subberthehut's search & download
+process by running it recursively inside a directory containing multiple video files
 
 *Tested under Arch Linux (Antergos)
 
+[Author]
+-saojeda (https://github.com/saojeda)
+
 [Dependencies]
 -subberthehut (https://github.com/mus65/subberthehut)
--Boost library (http://www.boost.org/)
+-Boost C++ library (http://www.boost.org/)
 
-[Credits]
--saojeda (author)
--vivithemage (majority of the filesystem code)
-(https://gist.github.com/vivithemage/9517678)
+[Installation]
+$ g++ sth-helper.cpp -o sth-helper -lboost_filesystem -lboost_system (compile)
+$ sudo mv sth-helper /usr/bin (move to /usr/bin in order to allow it to execute everywhere)
 
-Released under MIT license
+[Usage]
+$ sth-helper (note: execute inside the desired directory containing video files)
 */
 
 using namespace std;
 using namespace boost::filesystem;
 
-int main(int argc, char *argv[]) {
-    // list all files in current directory.
-    path p(".");
-    directory_iterator end_itr;
-    string current_file, command;
+// Handler function for handling cancel event
+void handler(int signum) {
+  cout << endl << "+ Task terminated." << endl;
+  exit(signum);
+}
 
-    // cycle through the directory
-    for (directory_iterator itr(p); itr != end_itr; itr++) {
-        // If it's not a directory, list it. If you want to list directories too, just remove this check.
-        if (is_regular_file(itr->path())) {
-            // assign current file name to current_file and echo it out to the console.
-            current_file = itr->path().string();
-            // Check if the file is a supported video file, you can also add extra conditions for extra filenames
-            if (current_file.find(".mkv") != string::npos
-              || current_file.find(".mp4") != string::npos
-              || current_file.find(".m4v") != string::npos
-              || current_file.find(".avi") != string::npos
-              || current_file.find(".ogg") != string::npos) {
-              // Erase "spawn" characters in front of the filename
-              current_file.erase(0,2);
-              // Prepare command with the current filename
-              command = "subberthehut \"" + current_file + "\" --same-name --quiet";
-              cout << "[Processing file]: " << current_file << endl;
-              // Convert string to char in order for system() function to accept it as argument
-              const char *cmd = command.c_str();
-              system(cmd);
-              cout << endl;
-            }
-        }
+int main(int argc, char* argv[]) {
+  path p(".");
+  string command, filename;
+  int i = 0;
+
+  cout << "+ Task started. Press ctrl-c within 3 seconds to cancel" << endl;
+  // Install signal to handle cancel event
+  signal(SIGINT, handler);
+  // Wait for 3 seconds
+  sleep(3);
+  // Ignore subsequent cancel events
+  signal(SIGINT, SIG_IGN);
+
+  // Recursively explore the directory
+  for (directory_entry& x : recursive_directory_iterator(p)) {
+    filename = x.path().string();
+
+    // Check for possible video files,
+    if (filename.find(".mp4") != string::npos
+        || filename.find(".mkv") != string::npos
+        || filename.find(".m4v") != string::npos
+        || filename.find(".avi") != string::npos
+        || filename.find(".ogg") != string::npos
+        || filename.find(".flv") != string::npos
+        || filename.find(".webm") != string::npos) {
+        cout << "+ Processing: " << filename << endl;
+        // Prepare command with the given file name
+        command = "subberthehut \"" + filename + "\" --same-name --quiet";
+        system(command.c_str());
+        i++;
     }
-    cout << "Finished.\n";
-    return 0;
+  }
+
+  if (i > 0) cout << "+ Finished. Processed " << i << " files." << endl;
+  else cout << "+ Finished. No video files found!" << endl;
+
+  return 0;
 }
